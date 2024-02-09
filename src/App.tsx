078c1cc5d070311d9670f21256b75d45373df64e
Tofail Ahmed Sayem <atofail50@gmail.com>
2024-02-09 10:06:39 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-6xl font-bold text-amber-700 '>React + Redux + Tailwind + Shadcn</h1>
+    </>
+  )
+}
+
+export default App
